694a800fb69bcc1adfd39764167da01b675b5f06
zhou <zhouhaonan4@gmail.com>
2024-11-11 14:46:55 +0800

    chore: 修改打包配置

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  outputDir: 'docs',
+  /* 
+    为了使用 github page 的设置
+  */
+  publicPath: '/FE-useful-code/',
+  outputDir: 'docs', // 特意打包到这个目录，github page 除了根目录就只有这个目录
+  assetsDir: './',
 });
