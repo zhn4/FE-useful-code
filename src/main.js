62e35a94936110ee0959324bd3ecca713d84cb30
zhou <zhouhaonan4@gmail.com>
2025-10-27 13:59:47 +0800

    feat: 补充引入组件的设置

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/style/style.less'
+import 'simple-mind-map/dist/simpleMindMap.esm.css'
+import JsonViewer from 'vue-json-viewer'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
+Vue.use(JsonViewer)
 
 new Vue({
   router,
